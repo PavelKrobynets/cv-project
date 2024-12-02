8d8df3a4bfd676d6d33d6912afe25f3ee033d54c
Pavel <krobynetspavel@gmail.com>
2024-12-02 11:16:14 +0200

    test build

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  basePath: "/cvProject",
   reactStrictMode: true,
-  basePath: "/cv-project",
 };
 
 export default nextConfig;

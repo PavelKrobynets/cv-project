f0c706b5095569a74944f343d3769978f3bbe178
Pavel <krobynetspavel@gmail.com>
2024-12-02 11:42:53 +0200

    fixing img deploy bug

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
   output: "export",
   basePath: "/cvProject",
   reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
